3cd84790a8c32ce4bc240a60832d0d3c0d5cbed2
rl-everly <lauren.vlam@gmail.com>
2015-03-27 20:48:21 -0600

    Added version number.

--- school-panels.php
+++ school-panels.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Project Viper</title>
+    <title>Project Viper v0.1</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap-theme.min.css">
     <link rel="stylesheet" href="school-panels.css" type="text/css">
